4bc7da8c350d9b69c106bba104c7cbfde1128675
Amreen <6229597@edu.vaniercollege.qc.ca>
2023-11-26 22:30:12 -0500

    Modify Student class

--- src/main/java/org/amreen/dto/Student.java
+++ src/main/java/org/amreen/dto/Student.java
@@ -1,11 +1,41 @@
 package org.amreen.dto;
 
+import java.util.Arrays;
+
 public class Student {
-    private String fname;
-    private String lname;
+    private static final int MAX_COURSE_NUM = 5;
+    private static int nextId = 1;
+
+    private String firstName;
+    private String lastName;
     private String id;
-    private int nextId;
     private int courseNum;
     private Course[] courses;
     private Department department;
+
+    /**
+     *
+     * @param lastName Last name of the student
+     * @param firstName First name of the student
+     * @param department Department of the student
+     */
+    public Student(String lastName, String firstName, Department department) {
+        this.id = String.format("S%03d", nextId++);
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.department = department;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", id='" + id + '\'' +
+                ", nextId=" + nextId +
+                ", courseNum=" + courseNum +
+                ", courses=" + Arrays.toString(courses) +
+                ", department=" + department +
+                '}';
+    }
 }
