423b8f0991a0b5670bb69123fe37a04acb8e1bd4
Amreen <6229597@edu.vaniercollege.qc.ca>
2023-11-26 22:45:56 -0500

    Modify Department class

--- src/main/java/org/amreen/dto/Department.java
+++ src/main/java/org/amreen/dto/Department.java
@@ -1,7 +1,26 @@
 package org.amreen.dto;
 
 public class Department {
+    private static int nextId = 1;
+
     private String id;
     private String departmentName;
-    private int nextId;
+
+    /**
+     *
+     * @param departmentName Name of the department
+     */
+    public Department(String departmentName) {
+        this.id = String.format("D%03d", nextId++);
+        this.departmentName = departmentName;
+    }
+
+    @Override
+    public String toString() {
+        return "Department{" +
+                "id='" + id + '\'' +
+                ", departmentName='" + departmentName + '\'' +
+                ", nextId=" + nextId +
+                '}';
+    }
 }
