408c9a7f5f4c1e621a8c14b4ab94ced5d01a8048
Amreen <6229597@edu.vaniercollege.qc.ca>
2023-11-26 22:18:44 -0500

    Modify Teacher class

--- src/main/java/org/amreen/dto/Teacher.java
+++ src/main/java/org/amreen/dto/Teacher.java
@@ -1,8 +1,33 @@
 package org.amreen.dto;
 
 public class Teacher {
-    private String lname;
-    private String fname;
+    private static int nextId = 1;
+
+    private String lastName;
+    private String firstName;
     private String id;
     private Department department;
+
+    /**
+     *
+     * @param lastName Last name of teacher
+     * @param firstName First name of teacher
+     * @param department Teacher's department
+     */
+    public Teacher(String lastName, String firstName, Department department) {
+        this.id = String.format("T%03d", nextId++);
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.department = department;
+    }
+
+    @Override
+    public String toString() {
+        return "Teacher{" +
+                "lastName='" + lastName + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", id='" + id + '\'' +
+                ", department=" + department +
+                '}';
+    }
 }
